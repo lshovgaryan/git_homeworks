7857720afe3cc2d8b2b8171a16dca3d0c145057f
aren <aren@aren-PC>
2020-05-28 15:39:02 +0400

    Print geometric shape with asterix

---
+++ empty_square.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+     for(int a=0;a<10;a++)
+        {
+            cout<<"*";
+        }
+            cout<<endl;
+for(int b=0;b<5;b++)
+    {
+    for(int c=0;c<1;c++)
+        {
+                cout<<"*";
+        }
+        for(int d=0;d<8;d++)
+              {
+                cout<<" ";
+              }
+                cout<<"*"<<endl;
+       }
+for(int e=0;e<10;e++)
+ {
+ cout<<"*";
+ }
+cout<<endl;
+ return 0;
+}
+
